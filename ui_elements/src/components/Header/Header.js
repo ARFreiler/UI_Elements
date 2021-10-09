ec641339e30d3413f1d175befc3971252d63d4eb
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-09 13:13:15 -0400

    Set color palette for Header, Navbar, and Footer components.

--- ui_elements/src/components/Header/Header.js
+++ ui_elements/src/components/Header/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import '../Header/Header.css';
 import '../Navbar/Navbar';
+import Navbar from '../Navbar/Navbar';
 
 
 const Header = () => {
     return (
-        <div className="body">
-            <div className="container">
-                <div className="header">
-                    <h1 className="title">UI Elements</h1>
-                    <p className="tag">A collection of UI elements for webpage design.</p>
-
-                </div>
-
+        <div className="container">
+            <div className="header">
+                <h1 className="title">UI Elements</h1>
+                <p className="tag">A collection of UI elements for webpage design.</p>
             </div>
+            {/* <Navbar currentPage={currentPage} handlePageChange={handlePageChange} /> */}
         </div>
-    )
-}
+    );
+};
 
 export default Header;
