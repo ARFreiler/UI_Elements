ffb4bdf6a51e00a010c86ca6947e4f89797b4ad4
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-26 22:32:50 -0400

    Added styling to hidden search component.

--- ui_elements/src/components/WidgetElements/Widgets.js
+++ ui_elements/src/components/WidgetElements/Widgets.js
@@ -8,6 +8,7 @@ const Widgets = () => {
         <div className="widgets-container">
             <h2 className="widgets-text">Random Widgets Selection</h2>
             <div className="widgets-panels">
+                {/* <p>widget panel</p> */}
                 <div className="widget-one">
                     <HiddenSearch />
                 </div>
